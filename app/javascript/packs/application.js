86f61cb53458af551cb5ac8213cdbea7d051a96b
Jules Maregiano <jules.maregiano@gmail.com>
2020-11-20 13:47:52 +0100

    initial commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,26 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+
+import "dragula"
+import "simplebar"
+import "popper.js"
+import "bootstrap"
+import "apexcharts"
+import "chart.js"
+import "jquery-toast-plugin"
+import "daterangepicker"
+import "moment"
+import "jvectormap-next"
+
+// Uncomment to copy all static images under ../images to the output folder and reference
+// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+// or the `imagePath` JavaScript helper below.
+//
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
