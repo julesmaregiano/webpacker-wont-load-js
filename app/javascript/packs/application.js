7d3fa8ce13edde22f1e2d20fff0f9d3cc7f04843
Jules Maregiano <jules.maregiano@gmail.com>
2020-11-21 10:10:03 +0100

    push to git

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,16 +7,19 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 
+
 import "dragula"
 import "simplebar"
 import "popper.js"
-import "bootstrap"
 import "apexcharts"
 import "chart.js"
 import "jquery-toast-plugin"
-import "daterangepicker"
 import "moment"
 import "jvectormap-next"
+// import "daterangepicker/package.js"
+import "bootstrap"
+
+import '../stylesheets/application'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
